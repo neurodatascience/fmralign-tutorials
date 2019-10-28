bdb49f794aa90a6450d6fe9132ba65b24797b17f
Elizabeth DuPre <emd222@cornell.edu>
2019-10-28 15:49:48 -0400

    Inital commit with ds1545

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # fmralign-tutorials
+
 [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/neurodatascience/fmralign-tutorials/master)
 
 A collection of tutorials for functional alignment methods in fMRI data.
@@ -7,11 +8,12 @@ we recommend [BrainIAK](http://brainiak.org/),
 [fmralign](https://github.com/Parietal-INRIA/fmralign), or
 [pyMVPA](http://www.pymvpa.org/).
 
-Here, we demonstrate functional alignment methods using the publicly accessible [Pixar dataset](https://osf.io/5hju4/), generously shared by the Saxe group.
+Here, we demonstrate functional alignment methods using the publicly accessible [MTL dataset](https://osf.io/vgj7w/), generously shared by Aly and colleagues.
 For more information on the acquisition of this data set, please see their paper:
 
-> Richardson, H., Lisandrelli, G., Riobueno-Naylor, A., & Saxe, R. (2018). [Development of the social brain from age three to twelve years](https://www.nature.com/articles/s41467-018-03399-2). _Nature Communications_, _9(1)_, 1027.
+> Aly M, Chen J, Turk-Browne NB, & Hasson U (2018).
+  Learning naturalistic temporal structure in the posterior medial network.
+  _Journal of Cognitive Neuroscience_, 30(9): 1345-1365.
 
-Data were pre-processed using [fMRIPrep](https://fmriprep.readthedocs.io) version 1.3.0.post2.
-For the exact processing pipeline, please see the fMRIPrep-generated methods section,
-available [here](https://osf.io/wjtyq/).
+Data were pre-processed using [fMRIPrep](https://fmriprep.readthedocs.io) version 1.5.0rc1.
+For a full description of the pipeline, please see the Open Science Framework [README](https://osf.io/479pt/).
---
+++ fetch_data.py
@@ -0,0 +1,213 @@
+import os
+import numbers
+import warnings
+import numpy as np
+from sklearn.datasets.base import Bunch
+
+from nilearn._utils.numpy_conversions import csv_to_array
+from nilearn.datasets.utils import (_get_dataset_dir, _fetch_files)
+
+
+def _fetch_mtl_fmri_participants(data_dir, url, verbose):
+    """Helper function to fetch_mtl_fmri.
+    This function helps in downloading participant data from .tsv file
+    uploaded on Open Science Framework (OSF).
+
+    Parameters
+    ----------
+    data_dir: str
+        Path of the data directory. Used to force data storage in a specified
+        location. If None is given, data are stored in home directory.
+    url: str, optional
+        Override download URL. Used for test only (or if you setup a mirror of
+        the data). Default: None
+    verbose: int
+        Defines the level of verbosity of the output.
+    Returns
+    -------
+    participants : numpy.ndarray
+        Details each subject's sex and age.
+    """
+    dataset_name = 'mtl_fmri'
+    data_dir = _get_dataset_dir(dataset_name, data_dir=data_dir,
+                                verbose=verbose)
+
+    if url is None:
+        url = 'https://osf.io/3gm9p/download/'
+
+    files = [('participants.tsv', url, {'move': 'participants.tsv'})]
+    path_to_participants = _fetch_files(data_dir, files, verbose=verbose)[0]
+
+    # Load path to participants
+    dtype = [('participant_id', 'U12'), ('sex', 'U4'), ('age', '<f8')]
+    names = ['participant_id', 'sex', 'age']
+    participants = csv_to_array(path_to_participants, skip_header=True,
+                                dtype=dtype, names=names)
+    return participants
+
+
+def _fetch_mtl_fmri_functional(n_subjects, n_runs, data_dir, url, verbose):
+    """Helper function to fetch_mtl_fmri.
+    This function helps in downloading functional MRI data in Nifti
+    format and associated regressors for each subject.
+    The files are downloaded from Open Science Framework (OSF).
+    Parameters
+    ----------
+    n_subjects : numpy.ndarray
+        The number of participants to download. If None, all available
+        participants are downloaded. Default: None
+    n_runs : int
+        The number of runs to download for each participant. If None, all
+        available runs are downloaded. Default: None
+    data_dir: str
+        Path of the data directory. Used to force data storage in a specified
+        location. If None is given, data are stored in home directory.
+    url: str, optional
+        Override download URL. Used for test only (or if you setup a mirror of
+        the data). Default: None
+    verbose: int
+        Defines the level of verbosity of the output.
+    Returns
+    -------
+    func: list of str (Nifti files)
+        Paths to functional MRI data (4D) for each subject.
+    regressors: list of str (tsv files)
+        Paths to regressors related to each subject.
+    """
+    dataset_name = 'mtl_fmri'
+    data_dir = _get_dataset_dir(dataset_name, data_dir=data_dir,
+                                verbose=verbose)
+
+    if url is None:
+        url = 'https://osf.io/download/{}'
+
+    confounds = 'subj-{0}_task-movie_run-{1}_desc-reducedConfounds_regressors.tsv'
+    func = ('subj-{0}_task-movie_run-{1}' +
+            'space-MNI152NLin2009cAsym_desc-postproc_bold.nii.gz')
+
+    # The gzip contains unique download keys per Nifti file and confound
+    # pre-extracted from OSF. Required for downloading files.
+    # tutorial_directory = os.path.dirname(os.path.abspath(__file__))
+    tutorial_directory = '/home/emdupre/Desktop/dissertation/simulations'
+    dtype = [('participant_id', 'U12'), ('run_num', 'U12'),
+             ('key_bold', 'U24'), ('key_regressor', 'U24')]
+    names = ['participant_id', 'run_num', 'key_b', 'key_r']
+    # csv file contains download information related to OSF
+    osf_data = csv_to_array(os.path.join(tutorial_directory, "mtl_fmri.csv"),
+                            skip_header=True, dtype=dtype, names=names)
+
+    funcs = []
+    regressors = []
+
+    if n_subjects is None:
+        n_subjects = 3
+
+    for subj in range(1, n_subjects + 1):
+        subj_files = osf_data[osf_data['participant_id'] == str(subj).zfill(2)]
+        if n_runs is not None:
+            subj_files = subj_files[:n_runs]
+
+        for f in subj_files:
+            # Download regressors
+            confound_url = url.format(f['key_r'])
+            regressor_file = [(confounds.format(subj, f['run_num']),
+                            confound_url,
+                            {'move': confounds.format(subj, f['run_num'])})]
+            path_to_regressor = _fetch_files(data_dir, regressor_file,
+                                            verbose=verbose)
+            regressors.append(path_to_regressor)
+
+            # Download bold images
+            func_url = url.format(f['key_b'])
+            func_file = [(func.format(subj, f['run_num']), func_url,
+                        {'move': func.format(subj, f['run_num'])})]
+            path_to_func = _fetch_files(data_dir, func_file, verbose=verbose)[0]
+            funcs.append(path_to_func)
+    return funcs, regressors
+
+
+def fetch_mtl_fmri(n_subjects=None, n_runs=None,
+                   data_dir=None, resume=True, verbose=1):
+    """Fetch fMRI dataset targeting the medial temporal lobe (MTL)
+    during repeated movie-watching. Created by Aly and colleagues
+    and originally downloaded from OpenNeuro. See Notes below.
+
+    Parameters
+    ----------
+    n_subjects: int, optional (default None)
+        The number of subjects to load. If None, all the subjects are
+        loaded. Maximum of 3 subjects.
+    n_runs: int, optional (default None)
+        The number of runs to load per subject. If None, all the runs are
+        loaded. Maximum of 3 runs.
+    data_dir: str, optional (default None)
+        Path of the data directory. Used to force data storage in a specified
+        location. If None, data are stored in home directory.
+    resume: bool, optional (default True)
+        Whether to resume download of a partly-downloaded file.
+    verbose: int, optional (default 1)
+        Defines the level of verbosity of the output.
+
+    Returns
+    -------
+    data: Bunch
+        Dictionary-like object, the interest attributes are :
+        - 'func': list of str (Nifti files)
+            Paths to downsampled functional MRI data (4D) for each subject.
+        - 'confounds': list of str (tsv files)
+            Paths to confounds related to each subject.
+        - 'phenotypic': numpy.ndarray
+            Details each subject's sex and age.
+
+    Notes
+    -----
+    The original data is downloaded from OpenNeuro
+    https://openneuro.org/datasets/ds001545/versions/1.0.0
+
+    This fetcher downloads downsampled data that are available on Open
+    Science Framework (OSF). Located here: https://osf.io/vgj7w/files/
+    Preprocessing details: https://osf.io/479pt/
+    
+    References
+    ----------
+    Please cite this paper if you are using this dataset:
+    Aly M, Chen J, Turk-Browne NB, & Hasson U (2018).
+    Learning naturalistic temporal structure in the posterior medial network.
+    Journal of Cognitive Neuroscience, 30(9): 1345-1365.
+    https://www.mitpressjournals.org/doi/full/10.1162/jocn_a_01308
+    """
+
+    dataset_name = 'mtl_fmri'
+    data_dir = _get_dataset_dir(dataset_name, data_dir=data_dir,
+                                verbose=verbose)
+
+    # Participants data: ids, demographics, etc
+    participants = _fetch_mtl_fmri_participants(data_dir=data_dir,
+                                                url=None,
+                                                verbose=verbose)
+
+    max_subjects = len(participants)
+    if n_subjects is None:
+        n_subjects = max_subjects
+
+    if (isinstance(n_subjects, numbers.Number) and
+            ((n_subjects > max_subjects) or (n_subjects < 1))):
+        warnings.warn("Wrong value for n_subjects={0}. The maximum "
+                      "value will be used instead n_subjects={1}"
+                      .format(n_subjects, max_subjects))
+        n_subjects = max_subjects
+    
+    if (isinstance(n_runs, numbers.Number) and
+                ((n_runs > 3) or (n_runs < 1))):
+        warnings.warn("Wrong value for n_runs={0}. The maximum "
+                      "value will be used instead n_runs={1}"
+                      .format(n_subjects, 3))
+        n_runs = 3
+
+    funcs, regressors = _fetch_mtl_fmri_functional(n_subjects=n_subjects,
+                                                   n_runs=n_runs,
+                                                   data_dir=data_dir,
+                                                   url=None,
+                                                   verbose=verbose)
+
+    return Bunch(func=funcs, confounds=regressors, phenotypic=participants)
